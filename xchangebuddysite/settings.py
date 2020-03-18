195e7c80353824918a157f01ee49a9098fe292eb
Nyone Tombari Johnson <tom.nyone@gmail.com>
2020-03-18 17:57:52 +0100

    Update settings.py

--- xchangebuddysite/settings.py
+++ xchangebuddysite/settings.py
@@ -136,7 +136,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'tom.nyone@gmail.com'
-EMAIL_HOST_PASSWORD = 'Nwinka01##'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 #  EMAIL_USE_SSL = False
 
